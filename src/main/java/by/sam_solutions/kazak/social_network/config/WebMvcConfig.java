7071690a4704eec957bb2360b9dfb52d504b4712
Vadim Kazak <1kazak.vadim@gmail.com>
2021-12-02 15:03:44 +0300

    edited @ComponentScan

--- src/main/java/by/sam_solutions/kazak/social_network/config/WebMvcConfig.java
+++ src/main/java/by/sam_solutions/kazak/social_network/config/WebMvcConfig.java
@@ -6,8 +6,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackageClasses = WebMvcConfig.class)
+@ComponentScan(basePackages = "by.sam_solutions.kazak.social_network")
 public class WebMvcConfig {
 
 }
-
