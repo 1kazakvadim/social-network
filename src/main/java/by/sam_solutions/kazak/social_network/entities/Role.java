17afc2478d62f9d1e0fdb46cf984c49ddbab796e
Vadim Kazak <1kazak.vadim@gmail.com>
2021-12-02 15:06:13 +0300

    add Role entity

---
+++ src/main/java/by/sam_solutions/kazak/social_network/entities/Role.java
@@ -0,0 +1,65 @@
+package by.sam_solutions.kazak.social_network.entities;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private Integer id;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  public Role() {
+  }
+
+  public Role(Integer id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Role role = (Role) o;
+    return Objects.equals(id, role.id) &&
+        Objects.equals(name, role.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name);
+  }
+
+}
