4f94f403821a6782a76f14a251308bc11ddd2cce
Vadim Kazak <1kazak.vadim@gmail.com>
2021-12-04 22:14:58 +0300

    add .jsp support

--- src/main/java/by/sam_solutions/kazak/social_network/config/AppContext.java
+++ src/main/java/by/sam_solutions/kazak/social_network/config/AppContext.java
@@ -50,6 +50,7 @@ public class AppContext {
     properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
     properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
     properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+    properties.put("hibernate.connection.CharSet", environment.getRequiredProperty("hibernate.connection.CharSet"));
     return properties;
   }
 
--- src/main/java/by/sam_solutions/kazak/social_network/config/WebAppConfig.java
+++ src/main/java/by/sam_solutions/kazak/social_network/config/WebAppConfig.java
@@ -1,12 +1,32 @@
 package by.sam_solutions.kazak.social_network.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "by.sam_solutions.kazak.social_network")
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
+
+  @Bean
+  public InternalResourceViewResolver resolver() {
+    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+    resolver.setViewClass(JstlView.class);
+    resolver.setPrefix("WEB-INF/pages/");
+    resolver.setSuffix(".jsp");
+    return resolver;
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/resources/**")
+        .addResourceLocations("/resources/");
+  }
 
 }
