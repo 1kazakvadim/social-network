e3d8fb04e01f907b1fa2f7cc819d133e87a56217
Vadim Kazak <1kazak.vadim@gmail.com>
2021-12-05 21:08:48 +0300

    add looger in UserServiceImpl

--- src/main/java/by/sam_solutions/kazak/social_network/services/impl/UserServiceImpl.java
+++ src/main/java/by/sam_solutions/kazak/social_network/services/impl/UserServiceImpl.java
@@ -4,6 +4,8 @@ import by.sam_solutions.kazak.social_network.dao.UserDao;
 import by.sam_solutions.kazak.social_network.entities.User;
 import by.sam_solutions.kazak.social_network.services.UserService;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class UserServiceImpl implements UserService {
 
+  private final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
   private final UserDao userDao;
 
   public UserServiceImpl(UserDao userDao) {
@@ -19,21 +22,25 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public void saveOrUpdate(User user) {
+    logger.debug("saveOrUpdate({})", user);
     userDao.saveOrUpdate(user);
   }
 
   @Override
   public User getById(Long id) {
+    logger.debug("get user by id = {}", id);
     return userDao.getById(id);
   }
 
   @Override
   public List<User> getAll() {
+    logger.debug("get all users");
     return userDao.getAll();
   }
 
   @Override
   public void deleteById(Long id) {
+    logger.debug("delete user with id = {}", id);
     userDao.deleteById(id);
   }
 
