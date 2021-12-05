e399607cd144cf8152167d617521dbaf7956acc6
Vadim Kazak <1kazak.vadim@gmail.com>
2021-12-05 20:56:01 +0300

    add hibernate.connection.CharSet property

--- src/main/java/by/sam_solutions/kazak/social_network/config/AppContext.java
+++ src/main/java/by/sam_solutions/kazak/social_network/config/AppContext.java
@@ -29,8 +29,7 @@ public class AppContext {
   public LocalSessionFactoryBean sessionFactory() {
     LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
     sessionFactory.setDataSource(dataSource());
-    sessionFactory.setPackagesToScan(
-        "by.sam_solutions.kazak.social_network.entities");
+    sessionFactory.setPackagesToScan("by.sam_solutions.kazak.social_network.entities");
     sessionFactory.setHibernateProperties(hibernateProperties());
     return sessionFactory;
   }
@@ -50,7 +49,9 @@ public class AppContext {
     properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
     properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
     properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
-    properties.put("hibernate.connection.CharSet", environment.getRequiredProperty("hibernate.connection.CharSet"));
+    properties.put(
+        "hibernate.connection.CharSet",
+        environment.getRequiredProperty("hibernate.connection.CharSet"));
     return properties;
   }
 
