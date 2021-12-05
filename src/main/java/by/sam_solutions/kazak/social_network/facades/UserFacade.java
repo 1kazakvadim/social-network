38c0fa65d0bbca6aca5354adcfb4b7a9b835087a
Vadim Kazak <1kazak.vadim@gmail.com>
2021-12-05 21:11:46 +0300

    add UserFacade

---
+++ src/main/java/by/sam_solutions/kazak/social_network/facades/UserFacade.java
@@ -0,0 +1,31 @@
+package by.sam_solutions.kazak.social_network.facades;
+
+import by.sam_solutions.kazak.social_network.converters.UserConverter;
+import by.sam_solutions.kazak.social_network.dto.UserDTO;
+import by.sam_solutions.kazak.social_network.entities.User;
+import by.sam_solutions.kazak.social_network.services.UserService;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserFacade {
+
+  private final UserService userService;
+  private final UserConverter userConverter;
+
+  public UserFacade(UserService userService, UserConverter userConverter) {
+    this.userService = userService;
+    this.userConverter = userConverter;
+  }
+
+  public List<UserDTO> getAll() throws Exception {
+    List<UserDTO> userDTOS = new ArrayList<>();
+    List<User> users = userService.getAll();
+    for (User user : users) {
+      userDTOS.add((UserDTO) userConverter.convertSourceToTargetClass(user, User.class));
+    }
+    return userDTOS;
+  }
+
+}
