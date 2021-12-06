f0ca539d84d436a400b9d7a4dde98280a9f62258
Vadim Kazak <1kazak.vadim@gmail.com>
2021-12-06 11:13:52 +0300

    add support internationalization in en, ru

--- src/main/java/by/sam_solutions/kazak/social_network/config/WebAppConfig.java
+++ src/main/java/by/sam_solutions/kazak/social_network/config/WebAppConfig.java
@@ -1,11 +1,17 @@
 package by.sam_solutions.kazak.social_network.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -25,8 +31,27 @@ public class WebAppConfig implements WebMvcConfigurer {
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/resources/**")
-        .addResourceLocations("/resources/");
+    registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+  }
+
+  @Bean("messageSource")
+  public MessageSource messageSource() {
+    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+    messageSource.setBasenames("l10n/messages");
+    messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
+  }
+
+  @Bean
+  public LocaleResolver localeResolver() {
+    return new CookieLocaleResolver();
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+    localeChangeInterceptor.setParamName("lang");
+    registry.addInterceptor(localeChangeInterceptor);
   }
 
 }
