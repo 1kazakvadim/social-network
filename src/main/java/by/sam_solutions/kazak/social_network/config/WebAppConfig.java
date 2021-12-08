4b831b3c28733964d6a9ce3125a0869df2517e96
Vadim Kazak <1kazak.vadim@gmail.com>
2021-12-08 20:03:54 +0300

    beans were moved in AppContextConfig

--- src/main/java/by/sam_solutions/kazak/social_network/config/WebAppConfig.java
+++ src/main/java/by/sam_solutions/kazak/social_network/config/WebAppConfig.java
@@ -1,50 +1,19 @@
 package by.sam_solutions.kazak.social_network.config;
 
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "by.sam_solutions.kazak.social_network")
 public class WebAppConfig implements WebMvcConfigurer {
-
-  @Bean
-  public InternalResourceViewResolver resolver() {
-    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-    resolver.setViewClass(JstlView.class);
-    resolver.setPrefix("WEB-INF/pages/");
-    resolver.setSuffix(".jsp");
-    return resolver;
-  }
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-  }
-
-  @Bean("messageSource")
-  public MessageSource messageSource() {
-    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-    messageSource.setBasenames("l10n/messages");
-    messageSource.setDefaultEncoding("UTF-8");
-    return messageSource;
-  }
-
-  @Bean
-  public LocaleResolver localeResolver() {
-    return new CookieLocaleResolver();
   }
 
   @Override
