ffbe713b11b3b6a6be7d482125ab0b71de01d105
Vadim Kazak <1kazak.vadim@gmail.com>
2021-12-08 20:04:24 +0300

    add SecurityWebAppInitializer

---
+++ src/main/java/by/sam_solutions/kazak/social_network/config/SecurityWebAppInitializer.java
@@ -0,0 +1,7 @@
+package by.sam_solutions.kazak.social_network.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
