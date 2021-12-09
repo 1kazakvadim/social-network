1aa7217602dfc894c860824a07a1a460658a575e
Vadim Kazak <1kazak.vadim@gmail.com>
2021-12-09 13:31:06 +0300

    added UserPrincipal

---
+++ src/main/java/by/sam_solutions/kazak/social_network/entities/UserPrincipal.java
@@ -0,0 +1,59 @@
+package by.sam_solutions.kazak.social_network.entities;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserPrincipal implements UserDetails {
+
+  private final User user;
+
+  public UserPrincipal(User user) {
+    this.user = user;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    if (null != user.getRole() && !user.getRole().getName().isEmpty()) {
+      return Arrays.stream(user.getRole().getName().split(","))
+          .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+          .collect(Collectors.toList());
+    }
+    return new ArrayList<>();
+  }
+
+  @Override
+  public String getPassword() {
+    return user.getPassword();
+  }
+
+  @Override
+  public String getUsername() {
+    return user.getEmail();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return !user.isLocked();
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
+}
