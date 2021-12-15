37d3c37e844b5b301c6e45830f97db7057444289
Vadim Kazak <1kazak.vadim@gmail.com>
2021-12-15 10:53:50 +0300

    added mail.properties

--- src/main/java/by/sam_solutions/kazak/social_network/config/EmailSenderConfig.java
+++ src/main/java/by/sam_solutions/kazak/social_network/config/EmailSenderConfig.java
@@ -1,26 +1,34 @@
 package by.sam_solutions.kazak.social_network.config;
 
 import java.util.Properties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
+@PropertySource("classpath:mail.properties")
 public class EmailSenderConfig {
+
+  @Autowired
+  private Environment environment;
 
   @Bean
   public JavaMailSender mailSender() {
     JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-    mailSender.setHost("smtp.gmail.com");
-    mailSender.setPort(587);
-    mailSender.setUsername("SocialNetworkSAMProject@gmail.com");
-    mailSender.setPassword("12345678Q!");
+    mailSender.setHost(environment.getRequiredProperty("mail.host"));
+    mailSender.setPort(environment.getRequiredProperty("mail.port", Integer.class));
+    mailSender.setUsername(environment.getRequiredProperty("mail.username"));
+    mailSender.setPassword(environment.getRequiredProperty("mail.password"));
     Properties properties = mailSender.getJavaMailProperties();
-    properties.put("mail.transport.protocol", "smtp");
-    properties.put("mail.smtp.auth", "true");
-    properties.put("mail.smtp.starttls.enable", "true");
-    properties.put("mail.debug", "true");
+    properties.put("mail.transport.protocol", environment.getRequiredProperty("mail.protocol"));
+    properties.put("mail.smtp.auth", environment.getRequiredProperty("mail.smtp.auth"));
+    properties
+        .put("mail.smtp.starttls.enable", environment.getRequiredProperty("mail.smtp.starttls"));
+    properties.put("mail.debug", environment.getRequiredProperty("mail.debug"));
     return mailSender;
   }
 
