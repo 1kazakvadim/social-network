ed6f3793ec3a39240df1edb4444147a33ed16375
Vadim Kazak <1kazak.vadim@gmail.com>
2021-12-22 10:36:40 +0300

    added EncodingFilter

---
+++ src/main/java/by/sam_solutions/kazak/social_network/config/EncodingFilter.java
@@ -0,0 +1,20 @@
+package by.sam_solutions.kazak.social_network.config;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import org.springframework.web.filter.GenericFilterBean;
+
+public class EncodingFilter extends GenericFilterBean {
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response,
+      FilterChain filterChain) throws IOException, ServletException {
+    request.setCharacterEncoding("UTF-8");
+    response.setCharacterEncoding("UTF-8");
+    filterChain.doFilter(request, response);
+  }
+
+}
