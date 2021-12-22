2db7955959cdeaf96efba0049c9bbf8506a64102
Vadim Kazak <1kazak.vadim@gmail.com>
2021-12-22 10:32:04 +0300

    updated

--- src/main/java/by/sam_solutions/kazak/social_network/config/WebAppInitializer.java
+++ src/main/java/by/sam_solutions/kazak/social_network/config/WebAppInitializer.java
@@ -1,5 +1,8 @@
 package by.sam_solutions.kazak.social_network.config;
 
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.FrameworkServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -17,6 +20,14 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
   @Override
   protected String[] getServletMappings() {
     return new String[]{"/"};
+  }
+
+  @Override
+  protected FrameworkServlet createDispatcherServlet(WebApplicationContext servletAppContext) {
+    DispatcherServlet dispatcherServlet = (DispatcherServlet)
+        super.createDispatcherServlet(servletAppContext);
+    dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+    return dispatcherServlet;
   }
 
 }
