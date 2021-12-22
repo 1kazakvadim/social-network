55cf6ad4ffb2c4b1e132e277b1e00eecb7b23672
Vadim Kazak <1kazak.vadim@gmail.com>
2021-12-22 10:34:07 +0300

    added PasswordResetToken entity

---
+++ src/main/java/by/sam_solutions/kazak/social_network/entities/PasswordResetToken.java
@@ -0,0 +1,96 @@
+package by.sam_solutions.kazak.social_network.entities;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "password_reset_token")
+public class PasswordResetToken {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false, unique = true)
+  private Long id;
+
+  @Column(name = "token", nullable = false)
+  private String token;
+
+  @OneToOne
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @Column(name = "expiry_date", nullable = false)
+  private LocalDateTime expiryDate;
+
+  public PasswordResetToken() {
+  }
+
+  public PasswordResetToken(Long id, String token,
+      User user, LocalDateTime expiryDate) {
+    this.id = id;
+    this.token = token;
+    this.user = user;
+    this.expiryDate = expiryDate;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getToken() {
+    return token;
+  }
+
+  public void setToken(String token) {
+    this.token = token;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public LocalDateTime getExpiryDate() {
+    return expiryDate;
+  }
+
+  public void setExpiryDate(LocalDateTime expiryDate) {
+    this.expiryDate = expiryDate;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PasswordResetToken that = (PasswordResetToken) o;
+    return Objects.equals(id, that.id) &&
+        Objects.equals(token, that.token) &&
+        Objects.equals(user, that.user) &&
+        Objects.equals(expiryDate, that.expiryDate);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, token, user, expiryDate);
+  }
+
+}
